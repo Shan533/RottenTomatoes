751a6a1ed5d96abdb89e947a18c102f94af42ca7
Shan533 <790219068@qq.com>
2023-12-04 03:48:22 -0800

    initial commit

---
+++ client/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {ConfigProvider} from 'antd'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: '#F54291',
+        colorBorder:'#4E142E',
+      },
+    }}
+    >
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>,
+)
---
+++ client/src/pages/Login/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Form, Button } from "antd";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <div className="grid grid-cols-2 h-screen">
+      <div className="bg-primary"></div>
+
+      <div className="flex items-center justify-center">
+        <div className="w-[500px]">
+          <h1 className="text-2xl mb-2">Login To Your Account</h1>
+          <hr />
+          <Form
+            layout="vertical"
+            className="flex flex-col gap-5 mt-3"
+            onFinish={onFinish}
+          >
+            <Form.Item label="Email" name="email">
+              <input />
+            </Form.Item>
+            <Form.Item label="Password" name="password">
+              <input />
+            </Form.Item>
+
+            <div className="flex flex-col gap-5">
+              <Button type="primary" htmlType="submit" block>
+                Login
+              </Button>
+
+              <Link to="/register" className="mt-2">
+                Don't have an account? Register
+              </Link>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
